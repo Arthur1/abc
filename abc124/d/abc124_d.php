4e113bd9d1e44050a16444d88c5a6cec3fcd4adf
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2019-05-20 16:33:59 +0900

    acc

---
+++ abc124/d/abc124_d.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * ABC124
+ * A - Buttons
+ */
+
+// $s = trim(fgets(STDIN));
+// fscanf(STDIN, "%d %d", $a, $b);
+// $n = array_map('intval', explode(' ', trim(fgets(STDIN))));
+
+echo $result, PHP_EOL;
