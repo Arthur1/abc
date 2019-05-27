5a8a3564fc53bd8b40d218aa0d23db0311e941b6
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2019-05-27 21:38:14 +0900

    ABC127 Unsolved

--- abc127/d/abc127_d.php
+++ abc127/d/abc127_d.php
@@ -9,27 +9,25 @@
 // $n = array_map('intval', explode(' ', trim(fgets(STDIN))));
 
 fscanf(STDIN, "%d %d", $n, $m);
-
-$a = array_map('intval', explode(' ', trim(fgets(STDIN))));
-
-$b_list = [];
-$c_list = [];
+$a = explode(' ', trim(fgets(STDIN)));
 for ($i = 0; $i < $m; $i++) {
     fscanf(STDIN, "%d %d", $b_list[$i], $c_list[$i]);
 }
 
 sort($a);
 arsort($c_list);
-$tasks = [];
+$k = 0;
 foreach ($c_list as $i => $c) {
     for ($j = 0; $j < $b_list[$i]; $j++) {
-        $tasks[] = $c;
+        if ($k == $n) {
+            break;
+        }
+        $tasks[$k] = $c;
+        $k++;
     }
 }
 
-$task_num = min(count($tasks), $n);
-
-for ($i = 0; $i < $task_num; $i++) {
+for ($i = 0; $i < $k; $i++) {
     if ($a[$i] > $tasks[$i]) {
         break;
     }
