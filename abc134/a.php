81b1708673667a4a767dcd556c7b91d61cf5696b
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2020-12-09 11:20:05 +0900

    ac-library

--- abc134/a.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $s = trim(fgets(STDIN));
-// fscanf(STDIN, "%d %d", $a, $b);
-// $n = array_map('intval', explode(' ', trim(fgets(STDIN))));
-
-fscanf(STDIN, "%d", $r);
-echo 3 * $r * $r, PHP_EOL;
